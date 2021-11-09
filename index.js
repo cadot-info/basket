b298ff3c1241398e5a2d206422bf4644a3ab4170
michael <michael@cadot.info>
2021-11-09 10:25:06 +0000

    panier ok

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-var ButtonAddClass,ButtonRemClass,IdnbrProducts,ClassList,IdtotalProducts;
+var ButtonAddClass, ButtonRemClass, IdnbrProducts, ClassList, IdtotalProducts, IdnbrTotalProducts,lineobjet;
 
 const basket = {
-  //loop for take action on element with class ButtonClass and add like or not like on element
+  /* -------------------------------------------------------------------------- */
+  /*                       loop for take action and inits                       */
+  /* -------------------------------------------------------------------------- */
   init(
     init_buttonAddClass = 'addBasket',
     init_buttonRemClass = 'remBasket',
     init_IdnbrProducts = 'nbrProducts',
     init_ClassList = 'listProducts',
-    init_IdtotalProducts = 'totalProducts'
+    init_IdtotalProducts = 'totalPriceProducts',
+    init_IdnbrTotalProducts = 'nbrTotalProducts'
   ) {
     ButtonAddClass = init_buttonAddClass;
     ButtonRemClass = init_buttonRemClass;
-    IdnbrProducts=init_IdnbrProducts;
-    ClassList=init_ClassList;
-    IdtotalProducts=init_IdtotalProducts;
+    IdnbrProducts = init_IdnbrProducts;
+    ClassList = init_ClassList;
+    IdtotalProducts = init_IdtotalProducts;
+    IdnbrTotalProducts = init_IdnbrTotalProducts;
 
+    /* ----------------------------- init the cookie ---------------------------- */
     let cookie = checkACookieExists('basket');
     if (cookie == false) cookie = [];
-    // take action add for class elements
+    /* ------------------- take action add for class elements ------------------- */
     var elements = document.getElementsByClassName(ButtonAddClass);
     for (var i = 0; i < elements.length; i++) {
       elements[i].addEventListener(
@@ -28,55 +33,77 @@ const basket = {
         false
       );
     }
-     // take action rem for class elements
-     var elements = document.getElementsByClassName(ButtonRemClass);
-     for (var i = 0; i < elements.length; i++) {
-       elements[i].addEventListener(
-         'click',
-         function () {
-           basket.rem(this); //add action for change cookie
-         },
-         false
-       );
-     }
+    /* ------------------- take action rem for class elements ------------------- */
+    var elements = document.getElementsByClassName(ButtonRemClass);
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener(
+        'click',
+        function () {
+          basket.rem(this); //add action for change cookie
+        },
+        false
+      );
+    }
+    /* ----------------------- init presenttaion of line ----------------------- */
+    lineobjet = document.getElementById('listProducts').firstElementChild.cloneNode(true); //contain the presentation of objet
+
+    
+
+
+    /* ---------------------------- update the basket --------------------------- */
     basket.update();
   },
-  //clear cookie basket
+
+  /* -------------------------------------------------------------------------- */
+  /*                             clear cookie basket                            */
+  /* -------------------------------------------------------------------------- */
   clear() {
     document.cookie = 'basket=; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
   },
-  //get array of cookie basket or false
+
+  /* -------------------------------------------------------------------------- */
+  /*                     get array of cookie basket or false                    */
+  /* -------------------------------------------------------------------------- */
   get() {
     return checkACookieExists();
   },
-  //return parameters, for tests
+
+  /* -------------------------------------------------------------------------- */
+  /*                        return parameters, for tests                        */
+  /* -------------------------------------------------------------------------- */
   getParameters() {
-    return ButtonAddClass+ButtonRemClass+IdnbrProducts+ClassList+IdtotalProducts;
+    return ButtonAddClass + ButtonRemClass + IdnbrProducts + ClassList + IdtotalProducts + IdnbrTotalProducts;
   },
-  //add in cookie basket
+
+  /* -------------------------------------------------------------------------- */
+  /*                            add in cookie basket                            */
+  /* -------------------------------------------------------------------------- */
   add(objet) {
-    //create objet default
-    let obj={
-      "id":objet.dataset.id,
-      "name":objet.dataset.name,
-      "price":objet.dataset.price,
-      "opts":objet.dataset.opts,
-      "quantity":1
+    /* -------------------------- create objet default -------------------------- */
+    let obj = {
+      id: objet.dataset.id,
+      name: objet.dataset.name,
+      price: objet.dataset.price,
+      opts: objet.dataset.opts,
+      quantity: 1,
     };
-    //verification cookie exist
+    /* ------------------------ verification cookie exist ----------------------- */
     let cookie = checkACookieExists('basket');
-    if (cookie != false) {// si basket cookie exist
-      //loo on objet for seek id
-      let index=-1;
-      let num=0;
-      cookie.forEach(element => {
-        num+=0;
-        if(element.id==objet.dataset.id)index=num;
+    if (cookie != false) {
+      // si basket cookie exist
+      /* ------------------------ loo on objet for seek id ------------------------ */
+      let index = -1;
+      let num = 0;
+      cookie.forEach((element) => {
+        num += 0;
+        if (element.id == objet.dataset.id) index = num;
       });
-      if (index !== -1) //if it is already in the list
-        cookie[index].quantity+=1;
+      if (index !== -1)
+        //if it is already in the list
+        cookie[index].quantity += 1;
       else cookie.push(obj); //If we cookie but not we are added
-    } else {//if cookie don't exist
+    } else {
+      //if cookie don't exist
       cookie = [obj];
     }
     var d = new Date();
@@ -85,49 +112,98 @@ const basket = {
     basket.update();
     return cookie;
   },
-  //remove in cookie basket
+
+  /* -------------------------------------------------------------------------- */
+  /*                           remove in cookie basket                          */
+  /* -------------------------------------------------------------------------- */
   rem(objet) {
-    //verification cookie exist
+    /* ------------------------ verification cookie exist ----------------------- */
     let cookie = checkACookieExists('basket');
-    if (cookie != false) {// si basket cookie exist
+    if (cookie != false) {
+      // si basket cookie exist
       //loo on objet for seek id
-      let index=-1;
-      let num=0;
-      cookie.forEach(element => {
-        num+=0;
-        if(element.id==objet.dataset.id)index=num;
+      let index = -1;
+      let num = 0;
+      cookie.forEach((element) => {
+        num += 0;
+        if (element.id == objet.dataset.id) index = num;
       });
-      if (index !== -1){ //if it is already in the list
-        if(cookie[index].quantity-1==0)
-        cookie.splice(index, 1);
-        else
-        cookie[index].quantity-=1;
-        //add in cookie
+      if (index !== -1) {
+        //if it is already in the list
+        if (cookie[index].quantity - 1 == 0) cookie.splice(index, 1);
+        //remove object
+        else cookie[index].quantity -= 1; //lower quantity
+        /* ------------------------------ rem in cookie ----------------------------- */
         var d = new Date();
         d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * 360);
         document.cookie = 'basket=' + JSON.stringify(cookie) + ';path=/;expires=' + d.toGMTString();
         basket.update();
       }
-    } 
-   
+    }
+
     return cookie;
   },
-  //update in document the counter
+
+  /* -------------------------------------------------------------------------- */
+  /*                       update in document the counters                      */
+  /* -------------------------------------------------------------------------- */
   update() {
+    //remove childs
+    document.getElementById('listProducts').innerHTML="";
     let cookie = checkACookieExists('basket');
-    if (cookie!=false && typeof document.getElementById(IdnbrProducts) != 'undefined' && document.getElementById(IdnbrProducts) != null)
-    {
-      let total=0;
-      cookie.forEach(element=>{
-      total+=element.quantity;
-      })
-      
-      return (document.getElementById(IdnbrProducts).textContent = total);
-    }
+    if (cookie != false) {
+      /* --------------------------------- calcul --------------------------------- */
+      let total = 0;
+      let totaltype = 0;
+      let totalprice=0;
+      /* ---------------------------------- loop ---------------------------------- */
+      cookie.forEach((element) => {
+        //nbr total of objects
+        total += element.quantity;
+        //nbr type objetcts
+        totaltype += 1;
+        /* ---------------------------- create line objet --------------------------- */
+        let lineFull=lineobjet.cloneNode(true);
+        lineFull.getElementsByClassName('productName')[0].innerHTML = element.name;
+        lineFull.getElementsByClassName('productQuantity')[0].innerHTML = element.quantity;
+        //catch option with ¤
+        let res=""; //stocke opt visible
+        for (var e in JSON.parse(element.opts)) {
+          if(e.charAt(0)=='¤')
+          res+=e+',';
+        };
+        lineFull.getElementsByClassName('productOpts')[0].innerHTML =res.substring(-1);
+        lineFull.getElementsByClassName('productPrice')[0].innerHTML = element.quantity*element.price;
+        totalprice+=element.quantity*element.price;
+        
+        document.getElementById('listProducts').appendChild(lineFull)
+
+        
+      });
+      /* ----------------------------- update document ---------------------------- */
+      if (
+        typeof document.getElementById(IdnbrTotalProducts) != 'undefined' &&
+        document.getElementById(IdnbrTotalProducts) != null
+      )
+        document.getElementById(IdnbrTotalProducts).textContent = totaltype;
+      if (
+        typeof document.getElementById(IdnbrProducts) != 'undefined' &&
+        document.getElementById(IdnbrProducts) != null
+      )
+        document.getElementById(IdnbrProducts).textContent = total;
+        if (
+          typeof document.getElementById(IdtotalProducts) != 'undefined' &&
+          document.getElementById(IdtotalProducts) != null
+        )
+        document.getElementById(IdtotalProducts).textContent = totalprice.toFixed(2);
+      return true;
+    } else return false;
   },
 };
 
-//function for get false or array of cookie basket
+/* -------------------------------------------------------------------------- */
+/*              function for get false or array of cookie basket              */
+/* -------------------------------------------------------------------------- */
 function checkACookieExists() {
   if (document.cookie == undefined) return false;
   let field = document.cookie.split('; ').find((row) => row.startsWith('basket='));
--- test.js
+++ test.js
@@ -14,39 +14,53 @@ beforeAll(() => {
 test('var by default', () => {
   //init for tests
   basket.clear();
-  //test parameters
-  basket.init('aaa','zzz', 'yes', 'no', 'bbb');
-  expect(basket.getParameters()).toBe('aaazzzyesnobbb');
+  //test parameters not possible because lineobjet is modified
+  //basket.init('aaa','zzz', 'yes', 'no', 'bbb','cccc');
+  //expect(basket.getParameters()).toBe('aaazzzyesnobbbcccc');
   basket.init();
-    expect(basket.getParameters()).toBe("addBasketremBasketnbrProductslistProductstotalProducts");
+    expect(basket.getParameters()).toBe("addBasketremBasketnbrProductslistProductstotalPriceProductsnbrTotalProducts");
 
-  //test fucntion get if cookies not exist
-  expect(basket.get()).toBe(false);
   expect(document.getElementById('nbrProducts').textContent).toBe("");
   //add object
   document.querySelectorAll(".addBasket")[0].click();
-  expect(basket.get()).toStrictEqual([{"id": "prod-1", "name": "Flour of corn", "opts": "{\"¤delivered\":\"4 days\",\"color\":[\"green\",\"red\"]}", "price": "12.12", "quantity": 1}]);
+  expect(basket.get()).toStrictEqual([{"id": "prod-1", "name": "Flour of corn", "opts": "{\"¤delivered\":\"4 days\",\"color\":\"green\"}", "price": "12.12", "quantity": 1}]);
   expect(document.getElementById('nbrProducts').textContent).toBe("1");
+  expect(document.getElementById('nbrTotalProducts').textContent).toBe("1");
+  expect(document.getElementById('totalPriceProducts').textContent).toBe("12.12");
+    expect(document.getElementById('listProducts').innerHTML).toBe("<li><span class=\"productName\">Flour of corn</span><span class=\"productQuantity\">1</span><span class=\"productOpts\">¤delivered,</span><span class=\"productPrice\">12.12</span></li>");
+
   //add same object
   document.querySelectorAll(".addBasket")[0].click();
-  expect(basket.get()).toStrictEqual([{"id": "prod-1", "name": "Flour of corn", "opts": "{\"¤delivered\":\"4 days\",\"color\":[\"green\",\"red\"]}", "price": "12.12", "quantity": 2}]);
+  expect(basket.get()).toStrictEqual([{"id": "prod-1", "name": "Flour of corn", "opts": "{\"¤delivered\":\"4 days\",\"color\":\"green\"}", "price": "12.12", "quantity": 2}]);
   expect(document.getElementById('nbrProducts').textContent).toBe("2");
+  expect(document.getElementById('nbrTotalProducts').textContent).toBe("1");
+  expect(document.getElementById('listProducts').innerHTML).toBe("<li><span class=\"productName\">Flour of corn</span><span class=\"productQuantity\">2</span><span class=\"productOpts\">¤delivered,</span><span class=\"productPrice\">24.24</span></li>");
+  expect(document.getElementById('totalPriceProducts').textContent).toBe("24.24");
+
  //add other object
  document.querySelectorAll(".addBasket")[2].click();
- expect(basket.get()).toStrictEqual([{"id": "prod-1", "name": "Flour of corn", "opts": "{\"¤delivered\":\"4 days\",\"color\":[\"green\",\"red\"]}", "price": "12.12", "quantity": 2},{"id": "prod-3", "name": "wine bordeaux 2016", "opts": "{\"¤warning\":\"allergen sulfite\"}", "price": "1.11", "quantity": 1}]);
+ expect(basket.get()).toStrictEqual([{"id": "prod-1", "name": "Flour of corn", "opts": "{\"¤delivered\":\"4 days\",\"color\":\"green\"}", "price": "12.12", "quantity": 2},{"id": "prod-3", "name": "wine bordeaux 2016", "opts": "{\"¤warning\":\"allergen sulfite\",\"¤color\":\"purple\"}", "price": "1.11", "quantity": 1}]);
  expect(document.getElementById('nbrProducts').textContent).toBe("3");
+ expect(document.getElementById('nbrTotalProducts').textContent).toBe("2");
+ expect(document.getElementById('listProducts').innerHTML).toBe("<li><span class=\"productName\">Flour of corn</span><span class=\"productQuantity\">2</span><span class=\"productOpts\">¤delivered,</span><span class=\"productPrice\">24.24</span></li><li><span class=\"productName\">wine bordeaux 2016</span><span class=\"productQuantity\">1</span><span class=\"productOpts\">¤warning,¤color,</span><span class=\"productPrice\">1.11</span></li>");
+ expect(document.getElementById('totalPriceProducts').textContent).toBe("25.35");
+
  //del first object
  document.querySelectorAll(".remBasket")[2].click();
- expect(basket.get()).toStrictEqual([{"id": "prod-1", "name": "Flour of corn", "opts": "{\"¤delivered\":\"4 days\",\"color\":[\"green\",\"red\"]}", "price": "12.12", "quantity": 1},{"id": "prod-3", "name": "wine bordeaux 2016", "opts": "{\"¤warning\":\"allergen sulfite\"}", "price": "1.11", "quantity": 1}]);
+ expect(basket.get()).toStrictEqual([{"id": "prod-1", "name": "Flour of corn", "opts": "{\"¤delivered\":\"4 days\",\"color\":\"green\"}", "price": "12.12", "quantity": 1},{"id": "prod-3", "name": "wine bordeaux 2016", "opts": "{\"¤warning\":\"allergen sulfite\",\"¤color\":\"purple\"}", "price": "1.11", "quantity": 1}]);
  expect(document.getElementById('nbrProducts').textContent).toBe("2");
+ expect(document.getElementById('nbrTotalProducts').textContent).toBe("2");
+ expect(document.getElementById('listProducts').innerHTML).toBe("<li><span class=\"productName\">Flour of corn</span><span class=\"productQuantity\">1</span><span class=\"productOpts\">¤delivered,</span><span class=\"productPrice\">12.12</span></li><li><span class=\"productName\">wine bordeaux 2016</span><span class=\"productQuantity\">1</span><span class=\"productOpts\">¤warning,¤color,</span><span class=\"productPrice\">1.11</span></li>");
+ expect(document.getElementById('totalPriceProducts').textContent).toBe("13.23");
+
  //del first object
  document.querySelectorAll(".remBasket")[2].click();
- expect(basket.get()).toStrictEqual([{"id": "prod-3", "name": "wine bordeaux 2016", "opts": "{\"¤warning\":\"allergen sulfite\"}", "price": "1.11", "quantity": 1}]);
+ expect(basket.get()).toStrictEqual([{"id": "prod-3", "name": "wine bordeaux 2016", "opts": "{\"¤warning\":\"allergen sulfite\",\"¤color\":\"purple\"}", "price": "1.11", "quantity": 1}]);
+ expect(document.getElementById('listProducts').innerHTML).toBe("<li><span class=\"productName\">wine bordeaux 2016</span><span class=\"productQuantity\">1</span><span class=\"productOpts\">¤warning,¤color,</span><span class=\"productPrice\">1.11</span></li>");
+ expect(document.getElementById('totalPriceProducts').textContent).toBe("1.11");
+
 //verify count on document
  expect(document.getElementById('nbrProducts').textContent).toBe("1");
-  //  //test remove basketorite
-  //  expect(basket.change(1)).toStrictEqual([4, 3]);
-  //  expect(basket.update()).toBe(2);
-  //     //test on document
-  //  expect(document.getElementById('counter').textContent).toBe("2")
+ expect(document.getElementById('nbrTotalProducts').textContent).toBe("1");
+
 });
